620d694d2b5652aff329efe3364182ddb319a8ee
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2023-09-03 15:20:28 +0200

    created databuilder to build data from api if not in localstorage

---
+++ dataBuilder.js
@@ -0,0 +1,129 @@
+const config = {
+    baseUrl: 'https://pokeapi.co/api/v2/',
+    limit: 'limit=',
+    offset: 'offset=',
+}
+
+export class DataBuilder {
+    constructor(app) {
+        console.log('%c 0 constructor() ', 'background: #42A5F5; color: #fff;');
+        this.app = app;
+        this.appConfig;
+        this.pokemonsNumber;
+        this.pokemons;
+    };
+
+    async init(app) {
+        console.log('%c 1 init() ', 'background: #42A5F5; color: #fff;');
+        this.appConfig = app.appConfig;
+        this.pokemonsNumber = app.pokemonsNumber;
+        this.pokemons = app.pokemons;
+        await this.getPokemonData();
+    }
+
+    async fetchAPI(fetchUrl) {
+        try {
+            const res = await fetch(fetchUrl);
+            if (!res.ok) {
+                console.error('Res not ok :(');
+                throw new Error(`Http error: ${res.status}`);
+            }
+            const json = await res.json();
+            return json;
+
+        } catch (error) {
+            console.error('Catch error');
+            console.error(error);
+        }
+    }
+
+    async getPokemonData() {
+        console.log('%c 2 getPokemonData() ', 'background: #42A5F5; color: #fff;');
+        console.log('---', this.pokemons);
+
+        const incorrectNames = await this.getPokemonCodeNames();
+
+        await this.getPokemonCorrectNames(incorrectNames);
+        await this.getPokemonTypes();
+        await this.getPokemonGenerations();
+
+        localStorage.setItem("PokemonsData", JSON.stringify(this.pokemons));
+
+        console.log('%c END ', 'background: #E53935; color: #fff;');
+    }
+
+    async getPokemonCodeNames() {
+        console.log('%c 3 getPokemonNames() ', 'background: #42A5F5; color: #fff;');
+
+        const incorrectNames = [];
+
+        await this.fetchAPI(this.appConfig['pokemon-species'] + '?' + config.limit + this.pokemonsNumber).then(res => {
+            res.results.forEach((pokemon,i) => {
+                if (/\-/.test(pokemon.name)) incorrectNames.push(pokemon.name);
+                this.pokemons[pokemon.name] = {
+                    id: i+1,
+                    name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
+                };
+            });
+        });
+        return incorrectNames;
+    }
+
+    async getPokemonCorrectNames(incorrectNames) {
+        console.log('%c 4 getPokemonCorrectNames() ', 'background: #42A5F5; color: #fff;');
+
+        for (const name of incorrectNames) {
+            await this.fetchAPI(this.appConfig['pokemon-species'] + name).then(res => {
+                this.pokemons[name].name = (res.names.find((object) => object.language.name === "en")).name;
+                console.log('correctNames:', name, this.pokemons[name].name);
+            });
+        };
+    }
+
+    async getPokemonTypes() {
+        console.log('%c 5 getPokemonTypes() ', 'background: #42A5F5; color: #fff;');
+
+        const res = await this.fetchAPI(this.appConfig['type']);
+        for (const type of res.results) {
+            const res = await this.fetchAPI(type.url)
+            for (const elem of res.pokemon) {
+                if (!this.pokemons[elem.pokemon.name]) continue;
+                if (!this.pokemons[elem.pokemon.name].type) this.pokemons[elem.pokemon.name].type = [];
+                this.pokemons[elem.pokemon.name].type[elem.slot-1] = type.name;
+            };
+            console.log('types:', type);
+        };
+
+        
+        console.log('%c 5.1 getPokemonTypes() ', 'background: #42A5F5; color: #fff;');
+
+        const missingType = [];
+        for (const [codeName, obj] of Object.entries(this.pokemons)) {
+            if (!obj.type) missingType.push(obj)
+        }
+
+        for (const obj of missingType) {
+            const res = await this.fetchAPI(this.appConfig['pokemon'] + obj.id)
+            res.types.forEach(elem => {
+                if (!obj.type) obj.type = []
+                obj.type[elem.slot-1] = elem.type.name;
+            })
+            console.log(obj.name, obj.type);
+        }
+
+    }
+
+    async getPokemonGenerations() {
+        console.log('%c 6 getPokemonGenerations() ', 'background: #42A5F5; color: #fff;');
+
+        const res = await this.fetchAPI(this.appConfig['generation']);
+        for (const gen of res.results) {
+            const res = await this.fetchAPI(gen.url)
+            for (const pokemon of res.pokemon_species) {
+                if (!this.pokemons[pokemon.name]) continue;
+                this.pokemons[pokemon.name].generation = gen.name;
+            };
+            console.log('gen:', gen);
+        };
+    }
+}
--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,5 @@
+import { DataBuilder } from './dataBuilder.js';
+
 const config = {
     baseUrl: 'https://pokeapi.co/api/v2/',
     limit: 'limit=',
@@ -13,11 +15,12 @@ export class Pokedex {
         this.appLoader;
         this.pokemonsNumber = ''; // '' for all
         this.pokemons = {};
+        this.dataBuilder = new DataBuilder();
         this.init();
     };
 
     async init() {
-        // console.log('%c 1 getPokemonsSpeciesNumber() ', 'background: #1976D2; color: #fff;');
+        // console.log('%c 1 init() ', 'background: #7E57C2; color: #fff;');
         await this.buildApp();
         await this.getAppUrls();
         await this.getPokemonsSpeciesNumber();
@@ -41,17 +44,17 @@ export class Pokedex {
     }
 
     async getAppUrls() {
-        // console.log('%c 2 getAppUrls() ', 'background: #1976D2; color: #fff;');
+        // console.log('%c 2 getAppUrls() ', 'background: #7E57C2; color: #fff;');
         this.appConfig = await this.fetchAPI(config.baseUrl).then(res => res);
     }
 
     async getPokemonsSpeciesNumber() {
-        // console.log('%c 3 getPokemonsSpeciesNumber() ', 'background: #1976D2; color: #fff;');
+        // console.log('%c 3 getPokemonsSpeciesNumber() ', 'background: #7E57C2; color: #fff;');
         this.pokemonsNumber = await this.fetchAPI(this.appConfig['pokemon-species'] + '?' + config.limit + 1).then(res => res.count);
     }
 
     async checkPokemons() {
-        console.log('%c 4 checkPokemons() ', 'background: #1976D2; color: #fff;');
+        console.log('%c 4 checkPokemons() ', 'background: #7E57C2; color: #fff;');
 
         if (localStorage.getItem("PokemonsData")) this.pokemons = JSON.parse(localStorage.getItem("PokemonsData"));
         if (localStorage.getItem("PokemonsData") && Object.keys(this.pokemons).length === this.pokemonsNumber) {
@@ -59,93 +62,9 @@ export class Pokedex {
             this.pokemons = JSON.parse(localStorage.getItem("PokemonsData"));
         } else {
             console.log('Get new pokemon data and save localy');
-            await this.getPokemonData();
-            localStorage.setItem("PokemonsData", JSON.stringify(this.pokemons));
+            await this.dataBuilder.init(this);
         }
         this.insertList();
-    }
-
-    async getPokemonData() {
-        console.log('%c 5 getPokemonData() ', 'background: #1976D2; color: #fff;');
-
-        const incorrectNames = await this.getPokemonCodeNames();
-
-        await this.getPokemonCorrectNames(incorrectNames);
-        await this.getPokemonTypes();
-        await this.getPokemonGenerations();
-
-        
-
-        console.log('%c END ', 'background: #E53935; color: #fff;');
-    }
-
-    async getPokemonCodeNames() {
-        console.log('%c 5.1 getPokemonNames() ', 'background: #1976D2; color: #fff;');
-        const incorrectNames = [];
-        await this.fetchAPI(this.appConfig['pokemon-species'] + '?' + config.limit + this.pokemonsNumber).then(res => {
-            res.results.forEach((pokemon,i) => {
-                if (/\-/.test(pokemon.name)) incorrectNames.push(pokemon.name);
-                this.pokemons[pokemon.name] = {
-                    id: i+1,
-                    name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1),
-                };
-            });
-        });
-        return incorrectNames;
-    }
-
-    async getPokemonCorrectNames(incorrectNames) {
-        console.log('%c 5.2 getPokemonCorrectNames() ', 'background: #1976D2; color: #fff;');
-
-        for (const name of incorrectNames) {
-            await this.fetchAPI(this.appConfig['pokemon-species'] + name).then(res => {
-                this.pokemons[name].name = (res.names.find((object) => object.language.name === "en")).name;
-                console.log('correctNames:', name, this.pokemons[name].name);
-            });
-        };
-    }
-
-    async getPokemonTypes() {
-        console.log('%c 5.3 getPokemonTypes() ', 'background: #1976D2; color: #fff;');
-
-        const res = await this.fetchAPI(this.appConfig['type']);
-        for (const type of res.results) {
-            const res = await this.fetchAPI(type.url)
-            for (const elem of res.pokemon) {
-                if (!this.pokemons[elem.pokemon.name]) continue;
-                if (!this.pokemons[elem.pokemon.name].type) this.pokemons[elem.pokemon.name].type = [];
-                this.pokemons[elem.pokemon.name].type[elem.slot-1] = type.name;
-            };
-            console.log('types:', type);
-        };
-
-        const missingType = [];
-        for (const [codeName, obj] of Object.entries(this.pokemons)) {
-            if (!obj.type) missingType.push(obj)
-        }
-
-        for (const obj of missingType) {
-            const res = await this.fetchAPI(this.appConfig['pokemon'] + obj.id)
-            res.types.forEach(elem => {
-                if (!obj.type) obj.type = []
-                obj.type[elem.slot-1] = elem.type.name;
-            })
-        }
-
-    }
-
-    async getPokemonGenerations() {
-        console.log('%c 5.4 getPokemonGenerations() ', 'background: #1976D2; color: #fff;');
-
-        const res = await this.fetchAPI(this.appConfig['generation']);
-        for (const gen of res.results) {
-            const res = await this.fetchAPI(gen.url)
-            for (const pokemon of res.pokemon_species) {
-                if (!this.pokemons[pokemon.name]) continue;
-                this.pokemons[pokemon.name].generation = gen.name;
-            };
-            console.log('gen:', gen);
-        };
     }
 
     buildApp() {
