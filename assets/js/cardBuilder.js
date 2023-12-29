68465e3c0b254b39a3d2a2e45646d191e9a48060
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2023-12-29 15:38:41 +0100

    images added

--- assets/js/cardBuilder.js
+++ assets/js/cardBuilder.js
@@ -20,9 +20,14 @@ export class CardBuilder {
         //     console.log("Ustawiono domyślne zdjęcie: " + defaultImage);
         // }
 
+
+        // console.log(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1018.png`);
+        // <div class='card__image'><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${pokemon.name}" class="item__img" loading="lazy"></div>
+
         return `<div class="card loading">
         ${(typeof id && typeof id !== 'undefined') ? `<div class='card__id'>${id}</div>` : ''}
         ${(typeof name && typeof name !== 'undefined') ? `<div class='card__name'>${name}</div>` : ''}
+        ${(typeof name && typeof name !== 'undefined') ? `<img class='card__image'><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" class="item__img" alt="${name}" loading="lazy"></div>` : ''}
 </div>`;
     }
 
