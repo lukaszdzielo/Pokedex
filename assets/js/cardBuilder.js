7b7cfa32734d3a096f6fce99bdbddf8b31c734c9
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2023-12-29 15:57:26 +0100

    name fix

--- assets/js/cardBuilder.js
+++ assets/js/cardBuilder.js
@@ -5,13 +5,15 @@ export class CardBuilder {
 
     pattern(codeName, pokemon) {
         const { id } = pokemon;
-        const name = pokemon.name ? `${codeName.charAt(0).toUpperCase()}${codeName.slice(1)}` : pokemon.name;
+        const name = pokemon.name ? pokemon.name : `${codeName.charAt(0).toUpperCase()}${codeName.slice(1)}`;
         const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+        console.log(name);
 
         return `<div class="card loading">
         ${(typeof id && typeof id !== 'undefined') ? `<div class='card__id'>${id}</div>` : ''}
         ${(typeof name && typeof name !== 'undefined') ? `<div class='card__name'>${name}</div>` : ''}
-        ${(typeof name && typeof name !== 'undefined') ? `<img class='card__image'><img src="${imageUrl}" class="item__img" alt="${name}" loading="lazy" onerror="this.onerror=null;this.src='./assets/0.png';"></div>` : ''}
+        ${(typeof id && typeof id !== 'undefined') ? `<img class='card__image'><img src="${imageUrl}" class="item__img" alt="${name}" loading="lazy" onerror="this.onerror=null;this.src='./assets/0.png';"></div>` : ''}
 </div>`;
     }
 
